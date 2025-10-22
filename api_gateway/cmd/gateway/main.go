ddb9822c18a04091c5c225d399ff682918367264
nicitapa <nicapagodev@gmail.com>
2025-10-22 23:04:34 +0300

    make home work

--- api_gateway/cmd/gateway/main.go
+++ api_gateway/cmd/gateway/main.go
@@ -1,1 +1,18 @@
-package gateway
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	cfg := config.Load()
+
+	r := router.Setup(cfg)
+	log.Printf("API Gateway listening on %s", cfg.Port)
+	err := http.ListenAndServe(cfg.Port, r)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
--- api_gateway/internal/router/router.go
+++ api_gateway/internal/router/router.go
@@ -1,1 +1,29 @@
 package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/nicitapa/financial-accounting/api_gateway/internal/config"
+	"github.com/nicitapa/financial-accounting/api_gateway/internal/proxy"
+	"net/http"
+	"net/url"
+)
+
+func mount(r chi.Router, prefix string, target string) {
+	u, _ := url.Parse(target)
+	h := proxy.New(u)  // ваш http.Handler на базе httputil.NewSingleHostReverseProxy
+	r.Mount(prefix, h) // chi сам срежет prefix для h
+}
+
+func Setup(cfg *config.Config) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/ping", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	mount(r, "/auth", cfg.AuthServiceURL)
+	mount(r, "/api/products", cfg.ProductServiceURL)
+
+	return r
+}
